910f4cd1818afd64a3f7c3044e8533882f8673df
Meso Francis <118773373+MessoJ@users.noreply.github.com>
2025-07-08 14:34:40 +0300

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -35,7 +35,7 @@ const Contact = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://contactbackend-5ebxme6ro-messos-projects.vercel.app/api/contact', {
+      const response = await fetch('https://contactbackend-topaz.vercel.app/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
